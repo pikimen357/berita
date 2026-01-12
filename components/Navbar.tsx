bce5d3f47bb0d4bcf6678f049660dca416643c96
pikimen357 <vidkyadhipradana2004@mail.ugm.ac.id>
2026-01-12 18:52:38 +0700

    Update UI

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="px-6 py-6 sm:px-6 lg:px-8 bg-gradient-to-r from-slate-900 to-slate-800 h-20">
+    <nav className="px-6 py-6 sm:px-6 lg:px-8 bg-linear-to-r from-slate-900 to-slate-800 h-20">
       <div className="max-w-6xl mx-auto flex items-center justify-between">
         <Link href="/" className="text-2xl font-bold text-white hover:text-blue-400 transition duration-300">
           Tech Blog
@@ -11,7 +11,7 @@ export default function Navbar() {
           <li>
             <Link
               href="/"
-              className="text-gray-200 hover:text-blue-400 font-semibold transition duration-200"
+              className="text-gray-200 hover:text-blue-400  active:underline font-semibold transition duration-200"
             >
               Home
             </Link>
@@ -19,7 +19,7 @@ export default function Navbar() {
           <li>
             <Link
               href="/blog"
-              className="text-gray-200 hover:text-blue-400 font-semibold transition duration-200"
+              className="text-gray-200 hover:text-blue-400  active:underline font-semibold transition duration-200"
             >
               Blog
             </Link>
@@ -27,7 +27,7 @@ export default function Navbar() {
           <li>
             <Link
               href="/about"
-              className="text-gray-200 hover:text-blue-400 font-semibold transition duration-200"
+              className="text-gray-200 hover:text-blue-400 active:underline font-semibold transition duration-200"
             >
               About
             </Link>
@@ -35,7 +35,7 @@ export default function Navbar() {
           <li>
             <Link
               href="/contact"
-              className="text-gray-200 hover:text-blue-400 font-semibold transition duration-200"
+              className="text-gray-200 hover:text-blue-400  active:underline font-semibold transition duration-200"
             >
               Contact
             </Link>
